2393b0d23f4b17ecaac0d38adb267a37d23acdcf
Shriyanshgithub <gshriyansh703@gmail.com>
2025-02-08 13:48:19 +0530

    [Shriyansh Gupta] Refactor

--- Day6/src/main/java/org/capgemini/Day6_Assignment/Problem_1/DataSet.java
+++ Day6/src/main/java/org/capgemini/Day6_Assignment/Problem_1/DataSet.java
@@ -1,4 +1,12 @@
 package org.capgemini.Day6_Assignment.Problem_1;
 
 public class DataSet {
+    public final int[] data;
+
+     public DataSet(int size){
+         this.data = new int[size];
+         for(int i =0; i<size ; i++){
+             data[i] = i;
+         }
+     }
 }
